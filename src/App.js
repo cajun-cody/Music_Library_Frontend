5e9e889f1c5aa1375fc58452228654493293ff95
Cody <cemt5462@gmail.com>
2023-02-08 12:53:32 -0600

    Fixed server run issue. Created GetAllSongs async function and moved to MusicTable.jsx.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useState,} from 'react';
+import React, { useState } from 'react';
 import MusicTable from './Components/MusicTable/MusicTable';
+
 
 
 function App() {
 
-  const [entries, setEntries] = useState([{title: 'Hey Jude', artist: 'Beatles', album: 'White', release_date: '10/10/56', genre: 'rock'}])
-
+  const [songs, setSongs] = useState([])
+ 
   return (
     <div>
-      <App parentEntries={entries}/>
+      <MusicTable parentSongs={songs} setSongs={setSongs}/>
     </div>
   );
 }
--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,31 +1,45 @@
+import React, { useEffect } from "react";
+import axios from "axios";
 
 
 const MusicTable = (props) => {
+
+    useEffect(() => {
+        getAllSongs();
+      }, [])
+    
+      async function getAllSongs(){
+        const response = await axios.get('http://127.0.0.1:8000/api/music/');
+        props.setSongs(response.data);
+      }
+
     return ( 
-        <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Artist</th>
-            <th>Album</th>
-            <th>Release Date</th>
-            <th>Genre</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.parentEntries.map((entry) => {
-            return(
-              <tr>
-                <td>{entry.title}</td>
-                <td>{entry.artist}</td>
-                <td>{entry.album}</td>
-                <td>{entry.release_date}</td>
-                <td>{entry.genre}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+        <div>
+            <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.parentSongs.map((song) => {
+                return(
+                <tr>
+                    <td>{song.title}</td>
+                    <td>{song.artist}</td>
+                    <td>{song.album}</td>
+                    <td>{song.release_date}</td>
+                    <td>{song.genre}</td>
+                 </tr>
+                )
+            })}
+            </tbody>
+            </table>   
+        </div>
      );
 }
  

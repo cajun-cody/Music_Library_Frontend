8fbfde8f47af9021c1bcca87d5d60b9f2ea94d90
Cody <cemt5462@gmail.com>
2023-02-08 09:56:25 -0600

    Removed un-needed files. Created components folder. Created MusicTable. Need to fix server run error.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,} from 'react';
+import MusicTable from './Components/MusicTable/MusicTable';
+
 
 function App() {
+
+  const [entries, setEntries] = useState([{title: 'Hey Jude', artist: 'Beatles', album: 'White', release_date: '10/10/56', genre: 'rock'}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <App parentEntries={entries}/>
     </div>
   );
 }

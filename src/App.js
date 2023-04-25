78bd8e7544ff4e03d267de58bebe669705ed5d97
Cody <cemt5462@gmail.com>
2023-04-25 15:18:27 -0500

    fix:Updating production URL_HOST

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import NavBar from './Components/NavBar/NavBar';
-import MusicTable from './Components/MusicTable/MusicTable';
-import SearchBar from './Components/SearchBar/SearchBar';
-import NewSongForm from './Components/NewSongForm/NewSongForm.jsx';
-import axios from 'axios';
-import './App.css';
-
-
+import React, { useState, useEffect } from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import MusicTable from "./Components/MusicTable/MusicTable";
+import SearchBar from "./Components/SearchBar/SearchBar";
+import NewSongForm from "./Components/NewSongForm/NewSongForm.jsx";
+import axios from "axios";
+import "./App.css";
+import { URL_HOST } from "./urlHost";
 
 function App() {
+  const [songs, setSongs] = useState([]);
 
-  const [songs, setSongs] = useState([])
-
-/* UseEffect function to gather all songs from backend and distribute 
+  /* UseEffect function to gather all songs from backend and distribute 
 to other components */
   useEffect(() => {
-	getAllSongs();
-  }, [])
+    getAllSongs();
+  }, []);
 
-  async function getAllSongs(){
-	const response = await axios.get('http://127.0.0.1:8000/api/music/');
-	setSongs(response.data);
+  async function getAllSongs() {
+    const response = await axios.get(`${URL_HOST}/api/music/`);
+    setSongs(response.data);
   }
 
-  async function createSong(newSong){
-	let response = await axios.post('http://127.0.0.1:8000/api/music/', newSong);
-	if(response.status === 201){
-		await getAllSongs();
-	}
+  async function createSong(newSong) {
+    let response = await axios.post(`${URL_HOST}/api/music/`, newSong);
+    if (response.status === 201) {
+      await getAllSongs();
+    }
   }
 
-/* Function to search through the songs list */
+  /* Function to search through the songs list */
   function songResults(search) {
-	let filteredSongs = songs.filter((song) => {
-		if (song.title.toLowerCase().includes(search.toLowerCase()) || 
-			song.artist.toLowerCase().includes(search.toLowerCase()) ||
-			song.album.toLowerCase().includes(search.toLowerCase()) ||
-			song.genre.toLowerCase().includes(search.toLowerCase()) ){
-		return true;
-		}	
-	})
-	setSongs(filteredSongs)
+    let filteredSongs = songs.filter((song) => {
+      if (
+        song.title.toLowerCase().includes(search.toLowerCase()) ||
+        song.artist.toLowerCase().includes(search.toLowerCase()) ||
+        song.album.toLowerCase().includes(search.toLowerCase()) ||
+        song.genre.toLowerCase().includes(search.toLowerCase())
+      ) {
+        return true;
+      }
+    });
+    setSongs(filteredSongs);
   }
 
-  async function deleteSong(song){
-	let response = await axios.delete(`http://127.0.0.1:8000/api/music/${song.id}/`);
-	getAllSongs();
+  async function deleteSong(song) {
+    let response = await axios.delete(`${URL_HOST}/api/music/${song.id}/`);
+    getAllSongs();
   }
- 
+
   return (
-    <div className='container-page'>
-		<NavBar/>
-		<SearchBar songResults= {songResults}/>
-		<NewSongForm addNewSong={createSong}/>
-    	<MusicTable parentSongs={songs} deleteSongProp={deleteSong}  />
+    <div className="container-page">
+      <NavBar />
+      <SearchBar songResults={songResults} />
+      <NewSongForm addNewSong={createSong} />
+      <MusicTable parentSongs={songs} deleteSongProp={deleteSong} />
     </div>
   );
 }
---
+++ src/urlHost.js
@@ -0,0 +1,4 @@
+export const URL_HOST =
+  process.env.NODE_ENV === "production"
+    ? "https://cors-everywhere-me.herokuapp.com/http://gamermusiclibrarybackend-env.eba-93vz4uqi.us-east-2.elasticbeanstalk.com"
+    : "http://127.0.0.1:8000";
